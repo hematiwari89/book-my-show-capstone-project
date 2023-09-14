01ce91a9ca12893c24de3afaf1c06f802ca43507
hematiwari89 <142926201+hematiwari89@users.noreply.github.com>
2023-09-15 00:20:29 +0530

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,6 @@
+let moviesList = ['OMG 2', 'Gadar 2', 'Guardians of galaxy 3',];
+let slots = ['10:00 AM', '01:00 PM', '03:00 PM', "08:00 PM"];
+let seats = ['A1', 'A2', 'A3', 'A4', 'D1', 'D2'];
+exports.moviesList = moviesList;
+exports.slots = slots;
+exports.seats = seats;
